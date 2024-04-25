f39c66752b5127eddf8336232da8ab925d46f643
Fernando Balieiro Ferreira <fernando.balieiro@outlook.com>
2024-04-25 00:44:09 -0300

    conectado com o banco de dados

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let port = env::var("PORT").unwrap_or_else(|_| "3000".to_owned());
     let addr = format!("0.0.0.0:{}", port);
 
-    // let db_addr =
-    //     env::var("DATABASE_URL")
-    //         .expect("missing DATABASE_URL env");
+    let db_addr =
+        env::var("DATABASE_URL")
+            .expect("missing DATABASE_URL env");
 
-    // let pool = PgPoolOptions::new()
-    //     .max_connections(5)
-    //     .connect(&db_addr)
-    //     .await?;
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_addr)
+        .await?;
 
     let app = Router::new()
-        .route("/helloworld", get(|| async { "Hello, World!" }));
-        // .with_state(pool);
+        .route("/helloworld", get(|| async { "Hello, World!" }))
+        .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
